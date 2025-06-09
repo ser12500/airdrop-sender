0feb473b46f7f47c3e4ffde29411041817c4fd68
ser12500 <119889023+ser12500@users.noreply.github.com>
2025-06-09 12:53:44 +0300

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
                     <h1 className="font-bold text-2xl hidden md:block">Airdrop Sender</h1>
                 </a>
                 <a
-                    href="https://github.com/cyfrin/TSender"
+                    href="https://github.com/ser12500/airdrop-sender"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="p-1 rounded-lg bg-zinc-900 hover:bg-zinc-800 transition-colors border-2 border-zinc-600 hover:border-zinc-500 cursor-alias hidden md:block"
